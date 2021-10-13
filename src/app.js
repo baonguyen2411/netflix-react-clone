1ce70a0c8f0f1fe41c53f990619118556d570f3c
Baong <nbao5471@gmail.com>
2021-10-13 21:55:03 +0700

    [Create] - Create Home Page and Remove Old folder non necessary

--- src/app.js
+++ src/app.js
@@ -1,38 +1,18 @@
-import React, { memo, useState, useEffect } from 'react';
+import React, { memo } from 'react';
+
+import ProtectedRoutes from './components/router/ProtectedRoutes';
 
 import routes from './routes';
-
-import Sidebar from './components/layout/Sidebar';
-import ProtectedRoutes from './components/router/ProtectedRoutes';
 
 import './styles.scss';
 
 const App = () => {
-  const [isActivated, setIsActivated] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsAuthenticated(true);
-    }, [5000]);
-
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-    };
-  }, []);
-
   return (
-    <>
-      <Sidebar
-        isActivated={isActivated}
-        onToggle={() => setIsActivated(!isActivated)}
-      />
-      <main className={`main ${isActivated && 'active'}`}>
-        <ProtectedRoutes routes={routes} isAuthenticated={isAuthenticated} />
+    <div className="nf-app">
+      <main className={`nf-main`}>
+        <ProtectedRoutes routes={routes} />
       </main>
-    </>
+    </div>
   );
 };
 
