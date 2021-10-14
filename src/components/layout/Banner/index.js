1054557f354af288a6d5adb81d5616fd36e167f4
Baong <nbao5471@gmail.com>
2021-10-14 14:09:05 +0700

    [Create] - Create and style Banner component

--- src/components/layout/Banner/index.js
+++ src/components/layout/Banner/index.js
@@ -1,8 +1,27 @@
 import React, { memo } from 'react';
 // import PropTypes from 'prop-types'
 
+import './styles.scss';
+
 const Banner = () => {
-  return <div className="nf-banner" />;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  };
+
+  return (
+    <header className="nf-banner">
+      <div className="nf-banner__contents">
+        <h1 className="nf-banner__title">Movie Name</h1>
+        <div className="nf-banner__buttons">
+          <button className="nf-banner__button">Play</button>
+          <button className="nf-banner__button">My List</button>
+        </div>
+        <h1 className="nf-banner__description">{truncate('', 150)}</h1>
+      </div>
+
+      <div className="nf-banner__fadeBotton" />
+    </header>
+  );
 };
 
 Banner.propTypes = {};
